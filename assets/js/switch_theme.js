852bbbebdfcdc63db5bc4a91dc0f6795331b8f01
BigIskander <bigiskander@gmail.com>
2024-03-09 08:10:21 +0300

    Small change. Check if sessionStorage exists.

--- assets/js/switch_theme.js
+++ assets/js/switch_theme.js
@@ -1,19 +1,20 @@
 //Switch do dark | light theme before page load if set up
 if(sessionStorage) {
-    var theme = sessionStorage.getItem("theme")
+    var theme = undefined;
+    if(sessionStorage) theme = sessionStorage.getItem("theme")
     if(!theme || theme == "light") {
         document.body.className = ''
-        sessionStorage.setItem("theme", "light")
+        if(sessionStorage) sessionStorage.setItem("theme", "light")
     } else {
         document.body.className = 'dark'
-        sessionStorage.setItem("theme", "dark")
+        if(sessionStorage) sessionStorage.setItem("theme", "dark")
     }
 }
 
 //Swich theme by pressing button or link
 function switch_theme() {
     var theme = undefined;
-    if(sessionStorage) var theme = sessionStorage.getItem("theme")
+    if(sessionStorage) theme = sessionStorage.getItem("theme")
     if(!theme || theme == "light") {
         document.body.className = 'dark'
         if(sessionStorage) sessionStorage.setItem("theme", "dark")
