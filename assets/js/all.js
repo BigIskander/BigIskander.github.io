aa427b1ec8c5fb955a74f95e390cea1f6316a135
BigIskander <bigiskander@gmail.com>
2024-04-28 13:27:31 +0300

    Made simpler zoom in out function.

--- assets/js/all.js
+++ assets/js/all.js
@@ -39,13 +39,13 @@ function contentOverflow()
 }
 
 function pageOnResize() {
-  zoomOutImage();
+  // zoomOutImage();
   pageOnScrollDo();
   contentOverflow();
 }
 
 function pageOnScrollDo() {
-  zoomOutImage();
+  // zoomOutImage();
   //to top button
   body = document.querySelector("body");
   toTopButton = document.querySelector(".to_top");
@@ -113,121 +113,68 @@ function addZoomToImages() {
 }
 
 function zoomInImage(image) {
-    zoomOutImage();
-    zoomedImage = document.querySelector(".image_zoom");
-    zoomedImagePreload = document.querySelector(".image_zoom_preload");
-    zoomedImageLink = document.querySelector(".image_zoom_open_url");
-    zoomedImageLink.setAttribute("href", image.src);
-    // show load spinner
-    spinner = document.getElementById("loadingSpinner")
-    spinner.style.top = (window.scrollY + window.innerHeight/2 - 20) + "px"
-    spinner.style.left = (window.innerWidth/2 - 20) + "px"
-    // if larger image should load
-    if(image.currentSrc && image.currentSrc!=image.src) {
-      zoomedImagePreload.src = image.currentSrc
-      if(!zoomedImagePreload.complete) spinner.style.visibility = "visible"
-    } else {
-      zoomedImage.src = image.src
-      if(!zoomedImage.complete) spinner.style.visibility = "visible"
-    }
-    // calculate image position
-    if(image.naturalHeight > (window.innerHeight * 0.9)
-        || image.naturalWidth > (window.innerWidth * 0.9))
-    {
-        if(image.naturalHeight == image.naturalWidth)
-        {
-            zoomedImageHeight = window.innerHeight * 0.9;
-            zoomedImageWidth = window.innerWidth * 0.9;
-        } else if(image.naturalHeight > image.naturalWidth) {
-            zoomedImageHeight = window.innerHeight * 0.9;
-            zoomedImageWidth = image.naturalWidth * (zoomedImageHeight / image.naturalHeight);
-            if(zoomedImageWidth > (window.innerWidth * 0.9)) {
-              zoomedImageWidth = window.innerWidth * 0.9;
-              zoomedImageHeight = image.naturalHeight * (zoomedImageWidth / image.naturalWidth);
-            }
-        } else {
-            zoomedImageWidth = window.innerWidth * 0.9;
-            zoomedImageHeight = image.naturalHeight * (zoomedImageWidth / image.naturalWidth);
-            if(zoomedImageHeight > (window.innerHeight * 0.9)) {
-              zoomedImageHeight = window.innerHeight * 0.9;
-              zoomedImageWidth = image.naturalWidth * (zoomedImageHeight / image.naturalHeight);
-            }
-        }
-    } else {
-        zoomedImageHeight = image.naturalHeight;
-        zoomedImageWidth = image.naturalWidth;
-    }
-    topAdjust = zoomedImageHeight >= (window.innerHeight * 0.9)
-        ? window.innerHeight * 0.05 : (window.innerHeight - zoomedImageHeight) / 2;
-    leftAdjust = zoomedImageWidth >= (window.innerWidth * 0.9)
-        ? window.innerWidth * 0.05 : (window.innerWidth - zoomedImageWidth) / 2;
-    // set images position
-    zoomedImage.style.height = zoomedImageHeight + "px";
-    zoomedImage.style.width = zoomedImageWidth + "px";
-    zoomedImage.style.top = topAdjust + "px";
-    zoomedImage.style.left = leftAdjust + "px";
-    zoomedImagePreload.style.height = zoomedImageHeight + "px";
-    zoomedImagePreload.style.width = zoomedImageWidth + "px";
-    zoomedImagePreload.style.top = topAdjust + "px";
-    zoomedImagePreload.style.left = leftAdjust + "px";
-    // set parent element position
-    zoomedImage.parentElement.style.top = window.scrollY + "px";
-    zoomedImage.parentElement.style.left = window.scrollX + "px";
-    zoomedImage.parentElement.style.height = window.innerHeight + "px";
-    zoomedImage.parentElement.style.width = window.innerWidth + "px";
-    // show image
-    if(image.currentSrc && image.currentSrc!=image.src) {
-      zoomedImagePreload.style.visibility = "visible";
-      zoomedImagePreload.onload= () => { 
-        zoomedImage.src=image.src
-        if(!zoomedImage.complete) spinner.style.visibility = "visible"
-        zoomedImage.onload = () => {
-          zoomedImage.style.visibility = "visible"
-          zoomedImagePreload.style.visibility = "hidden"
-          spinner.style.visibility = "hidden"
-        }
-        zoomedImage.onerror = () => {
-          spinner.style.visibility = "hidden"
-        }
-      }
-      zoomedImagePreload.onerror = () => {
-        spinner.style.visibility = "hidden"
-      }
-    } else {
-      zoomedImage.style.visibility = "visible"
-      zoomedImage.onload = () => {    
-        spinner.style.visibility = "hidden"
-      }
-      zoomedImage.onerror = () => {
-        spinner.style.visibility = "hidden"
-      }
-    }
-    zoomedImage.parentElement.style.visibility = "visible";
-    zoomed=true;
-    // zoom out event listener
-    setTimeout(() => {
-      zoomedImage.addEventListener("click", zoomOutImage)
-      zoomedImagePreload.addEventListener("click", zoomOutImage)
-    }, 100);
+	zoomOutImage();
+	imageZoomDiv = document.querySelector(".image_zoom_div");
+	zoomedImage = imageZoomDiv.querySelector(".image_zoom");
+	zoomedImagePreload = imageZoomDiv.querySelector(".image_zoom_preload");
+	zoomedImageLink = imageZoomDiv.querySelector(".image_zoom_open_url");
+	zoomedImageLink.setAttribute("href", image.src);
+	loadingSpinner = imageZoomDiv.querySelector(".image_loading_spinner");
+	//
+	imageZoomDiv.style.top = window.scrollY + "px";
+	imageZoomDiv.style.left = window.scrollX + "px";
+	imageZoomDiv.style.visibility = "visible";
+	zoomed = true;
+	isImagePreload = false;
+	zoomedImage.onload = function() {
+		isImagePreload = true;
+		loadingSpinner.style.visibility = "hidden";
+		zoomedImagePreload.style.visibility = "hidden";
+		zoomedImage.style.visibility = "visible";
+	}
+	zoomedImage.onerror = function() {
+		isImagePreload = true;
+		loadingSpinner.style.visibility = "hidden";
+	}
+	zoomedImagePreload.onload = function() {
+		zoomedImage.src = image.src;
+		zoomedImagePreload.style.visibility = "visible";
+	}
+	zoomedImagePreload.onerror = function() {
+		isImagePreload = true;
+		loadingSpinner.style.visibility = "hidden";
+	}
+	if(image.currentSrc && image.currentSrc!=image.src) {
+		zoomedImagePreload.src = image.currentSrc;
+	} else {
+		zoomedImage.src = image.src;
+	}
+	setTimeout(function() {
+		if(!isImagePreload) loadingSpinner.style.visibility = "visible";
+	}, 200);
+	document.body.style.overflow = "hidden";
 }
 
 function zoomOutImage() {
-    if(zoomed) {
-      zoomedImageLink = document.querySelector(".image_zoom_open_url");
-      zoomedImageLink.removeAttribute("href");
-      spinner = document.getElementById("loadingSpinner")
-      spinner.style.visibility = "hidden"
-      zoomedImage = document.querySelector(".image_zoom");
-      zoomedImagePreload = document.querySelector(".image_zoom_preload");
-      zoomedImage.setAttribute("src", "");
-      zoomedImagePreload.setAttribute("src", "");
-      zoomedImage.style.visibility = "hidden";
-      zoomedImagePreload.style.visibility = "hidden";
-      zoomedImage.parentElement.style.visibility = "hidden";
-      zoomed=false;
-      zoomedImage.removeEventListener("click", zoomOutImage);
-      zoomedImagePreload.removeEventListener("click", zoomOutImage);
-    }
+	if(zoomed) {
+		imageZoomDiv = document.querySelector(".image_zoom_div");
+		zoomedImage = imageZoomDiv.querySelector(".image_zoom");
+		zoomedImagePreload = imageZoomDiv.querySelector(".image_zoom_preload");
+		zoomedImageLink = imageZoomDiv.querySelector(".image_zoom_open_url");
+		loadingSpinner = imageZoomDiv.querySelector(".image_loading_spinner");
+		//
+		imageZoomDiv.style.visibility = "hidden";
+		loadingSpinner.style.visibility = "hidden";
+		zoomedImagePreload.style.visibility = "hidden";
+		zoomedImage.style.visibility = "hidden";
+		zoomedImagePreload.removeAttribute("src");
+		zoomedImage.removeAttribute("src");
+		zoomedImageLink.removeAttribute("href");
+		document.body.style.removeProperty("overflow");
+		imageZoomDiv.style.removeProperty("top");
+		imageZoomDiv.style.removeProperty("left");
+		zoomed = false;
+	}
 }
 
 function show_hide_element(elementID){
